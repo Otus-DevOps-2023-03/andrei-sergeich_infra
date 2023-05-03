798b0dc69eb3a6ef5630ecfd0d9e0173448df020
Andrei Prokhorov <andrei-sergeich@yandex.ru>
2023-05-03 12:40:45 +0300

    Fix scripts

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -2,7 +2,7 @@
 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add - # После пайпа sudo не убрано - иначе не отработает добавление ключа
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-apt-get update
-apt-get install -y mongodb-org
+apt update
+apt install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
