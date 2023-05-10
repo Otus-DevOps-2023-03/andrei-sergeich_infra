7c52de9fb6ec6a366d92180b943a7ea12c67afc5
Andrei Prokhorov <andrei-sergeich@yandex.ru>
2023-05-10 20:40:19 +0300

    Add application deploying

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,9 +16,6 @@ provider "yandex" {
 
 resource "yandex_compute_instance" "app" {
   name = "reddit-app"
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
-  }
 
   resources {
     cores         = 2
@@ -37,5 +34,27 @@ resource "yandex_compute_instance" "app" {
     # Указан id подсети default-ru-central1-a
     subnet_id = "e9bru58ieckd54a89a5a"
     nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+  }
+
+  connection {
+    type        = "ssh"
+    host        = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user        = "ubuntu"
+    agent       = false
+    private_key = file("~/.ssh/id_ed25519")
+    # путь до приватного ключа
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
   }
 }
