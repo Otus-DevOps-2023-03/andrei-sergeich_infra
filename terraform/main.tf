bb5bb0a98ff25e7850bbb5872d0971aa5559a9bd
Andrei Prokhorov <andrei-sergeich@yandex.ru>
2023-05-11 23:48:21 +0300

    Add creating instances via count parameter

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,8 @@ provider "yandex" {
 }
 
 resource "yandex_compute_instance" "app" {
-  name = "reddit-app"
+  count = var.inst_count
+  name = "reddit-app-${count.index + 1}"
   zone = var.zone
 
   resources {
@@ -43,53 +44,7 @@ resource "yandex_compute_instance" "app" {
 
   connection {
     type        = "ssh"
-    host        = yandex_compute_instance.app.network_interface.0.nat_ip_address
-    user        = "ubuntu"
-    agent       = false
-    private_key = file(var.private_key_path)
-    # путь до приватного ключа
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
-}
-
-resource "yandex_compute_instance" "app2" {
-  name = "reddit-app2"
-  zone = var.zone
-
-  resources {
-    cores         = 2
-    memory        = 2
-    core_fraction = 5
-  }
-
-  boot_disk {
-    initialize_params {
-      # Указать id образа созданного в предыдущем домашем задании
-      image_id = var.image_id
-    }
-  }
-
-  network_interface {
-    # Указан id подсети default-ru-central1-a
-    subnet_id = var.subnet_id
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file(var.public_key_path)}"
-  }
-
-  connection {
-    type        = "ssh"
-    host        = yandex_compute_instance.app2.network_interface.0.nat_ip_address
+    host        = self.network_interface.0.nat_ip_address
     user        = "ubuntu"
     agent       = false
     private_key = file(var.private_key_path)
