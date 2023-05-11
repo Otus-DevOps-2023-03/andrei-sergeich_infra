8b3827410724212c1ef8bdce4b74daad3bdacc01
Andrei Prokhorov <andrei-sergeich@yandex.ru>
2023-05-11 21:17:51 +0300

    Add code for another one instance

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -32,4 +32,9 @@ resource "yandex_lb_target_group" "lb" {
     subnet_id = var.subnet_id
     address   = yandex_compute_instance.app.network_interface.0.ip_address
   }
+
+    target {
+    subnet_id = var.subnet_id
+    address   = yandex_compute_instance.app2.network_interface.0.ip_address
+  }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -59,3 +59,49 @@ resource "yandex_compute_instance" "app" {
     script = "files/deploy.sh"
   }
 }
+
+resource "yandex_compute_instance" "app2" {
+  name = "reddit-app2"
+  zone = var.zone
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 5
+  }
+
+  boot_disk {
+    initialize_params {
+      # Указать id образа созданного в предыдущем домашем задании
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    # Указан id подсети default-ru-central1-a
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    host        = yandex_compute_instance.app2.network_interface.0.nat_ip_address
+    user        = "ubuntu"
+    agent       = false
+    private_key = file(var.private_key_path)
+    # путь до приватного ключа
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
