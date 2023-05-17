289f173b7ec64e32541109e613e1decfbfec0525
Andrei Prokhorov <andrei-sergeich@yandex.ru>
2023-05-17 20:29:34 +0300

    Split the infrastructure configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,60 +15,26 @@ provider "yandex" {
   zone                     = var.zone
 }
 
-resource "yandex_compute_instance" "app" {
-  count = var.inst_count
-  name  = "reddit-app-${count.index + 1}"
-  zone  = var.zone
+# resource "yandex_compute_instance" "app" {
+#   count = var.inst_count
+#   name  = "reddit-app-${count.index + 1}"
+#   zone  = var.zone
 
-  resources {
-    cores         = 2
-    memory        = 2
-    core_fraction = 5
-  }
+#   connection {
+#     type        = "ssh"
+#     host        = self.network_interface.0.nat_ip_address
+#     user        = "ubuntu"
+#     agent       = false
+#     private_key = file(var.private_key_path)
+#     # путь до приватного ключа
+#   }
 
-  boot_disk {
-    initialize_params {
-      # Указать id образа созданного в предыдущем домашем задании
-      image_id = var.image_id
-    }
-  }
+#   provisioner "file" {
+#     source      = "files/puma.service"
+#     destination = "/tmp/puma.service"
+#   }
 
-  network_interface {
-    # Указан id создаваемой подсети
-    subnet_id = yandex_vpc_subnet.app-subnet.id
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file(var.public_key_path)}"
-  }
-
-  connection {
-    type        = "ssh"
-    host        = self.network_interface.0.nat_ip_address
-    user        = "ubuntu"
-    agent       = false
-    private_key = file(var.private_key_path)
-    # путь до приватного ключа
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
-}
-
-resource "yandex_vpc_network" "app-network" {
-  name = "reddit-app-network"
-}
-
-resource "yandex_vpc_subnet" "app-subnet" {
-  name           = "reddit-app-subnet"
-  zone           = var.zone
-  network_id     = yandex_vpc_network.app-network.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
-}
+#   provisioner "remote-exec" {
+#     script = "files/deploy.sh"
+#   }
+# }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,12 +2,12 @@
 #   value = yandex_compute_instance.app[*].network_interface.0.nat_ip_address
 # }
 
-output "external_ip_address_apps" {
-  value = [
-    for inst in yandex_compute_instance.app:
-    "IP of ${inst.name} is ${inst.network_interface.0.nat_ip_address}"
-  ]
-}
+# output "external_ip_address_apps" {
+#   value = [
+#     for inst in yandex_compute_instance.app :
+#     "IP of ${inst.name} is ${inst.network_interface.0.nat_ip_address}"
+#   ]
+# }
 
 # output "external_ip_address_lb" {
 #   value = yandex_lb_network_load_balancer.lb.listener[*].external_address_spec[*].address
@@ -16,3 +16,10 @@ output "external_ip_address_apps" {
 # output "external_ip_address_lb" {
 #   value = tolist(tolist(yandex_lb_network_load_balancer.lb.listener)[0].external_address_spec)[0].address
 # }
+
+output "external_ip_address_app" {
+  value = yandex_compute_instance.app.network_interface.0.nat_ip_address
+}
+output "external_ip_address_db" {
+  value = yandex_compute_instance.db.network_interface.0.nat_ip_address
+}
