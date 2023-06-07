78cf667a3d8fea0d3ba02c45c29e780c9288d337
Andrei Prokhorov <andrei-sergeich@yandex.ru>
2023-06-07 20:56:06 +0300

    Fix: add ssh_user var in module call

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -11,6 +11,7 @@ module "vpc" {
 
 module "app" {
   source           = "../modules/app"
+  ssh_user         = var.ssh_user
   public_key_path  = var.public_key_path
   app_disk_image   = var.app_disk_image
   private_key_path = var.private_key_path
@@ -21,6 +22,7 @@ module "app" {
 
 module "db" {
   source           = "../modules/db"
+  ssh_user         = var.ssh_user
   public_key_path  = var.public_key_path
   db_disk_image    = var.db_disk_image
   private_key_path = var.private_key_path
